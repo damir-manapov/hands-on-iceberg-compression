ed3273e04a3cf662b98c4fe2b9dbeae0b7fbab35
Damir <tebuchet@gmail.com>
2025-10-07 23:22:26 +0300

    measures

--- src/config/load.ts
+++ src/config/load.ts
@@ -1,12 +1,11 @@
 // Load plan
 export const LOAD = {
   startId: Number(process.env.START_ID ?? "1"),
-  totalRows: Number(process.env.TOTAL_ROWS ?? `${100_000_000}`),   // e.g., 10M
-  // totalRows: Number(process.env.TOTAL_ROWS ?? `${10_000_000}`), // e.g., 10M
-  // totalRows: Number(process.env.TOTAL_ROWS ?? `${1_000_000}`), // e.g., 10M
-  // totalRows: Number(process.env.TOTAL_ROWS ?? `${10_000_000}`),   // e.g., 10M
-  // totalRows: Number(process.env.TOTAL_ROWS ?? `${20_000_000}`),   // e.g., 10M
-  // totalRows: Number(process.env.TOTAL_ROWS ?? `${1_000_000}`),   // e.g., 10M
+  // totalRows: Number(process.env.TOTAL_ROWS ?? `${1_000_000_000}`),   // e.g., 10M
+  // totalRows: Number(process.env.TOTAL_ROWS ?? `${100_000_000}`),
+  totalRows: Number(process.env.TOTAL_ROWS ?? `${10_000_000}`),
+  // totalRows: Number(process.env.TOTAL_ROWS ?? `${1_000_000}`),
+  // totalRows: Number(process.env.TOTAL_ROWS ?? `${100_000}`),
   batchRows: Number(process.env.BATCH_ROWS ?? `${100_000}`), // e.g., 5M per insert
   concurrency: Number(process.env.CONCURRENCY ?? "4"),
   createBaseSchema: (process.env.CREATE_BASE_SCHEMA ?? "true") === "true",
@@ -18,6 +17,6 @@ export const LOAD = {
 };
 
 // 1ml -> 300mb, 1.1min
-// 10ml -> 3gb
-// 100ml -> 30gb
-// billion -> 300gb
+// 10ml -> 3gb, ~7min
+// 100ml -> 30gb, ~2h
+// billion -> 300gb, ~19h
